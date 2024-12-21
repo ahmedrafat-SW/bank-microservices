4ff4c5763969547e6debb6701d46be2d981d97ec
Ahmed Rafat <ahmed.rafat.abdelbar@gmail.com>
2024-12-21 13:16:29 +0200

    updated based entity for enabling audit

--- src/main/java/org/rafat/dev/accounts/model/BaseEntity.java
+++ src/main/java/org/rafat/dev/accounts/model/BaseEntity.java
@@ -1,31 +1,38 @@
 package org.rafat.dev.accounts.model;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @MappedSuperclass
 @Setter
 @Getter
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
 
     @Column(name = "created_at")
-    @CreationTimestamp
+    @CreatedDate
     private LocalDateTime createdAt;
 
     @Column(name = "created_by")
+    @CreatedBy
     private String createdBy;
 
     @Column(name = "updated_at")
-    @UpdateTimestamp
+    @LastModifiedDate
     private LocalDateTime updatedAt;
 
     @Column(name = "updated_by")
+    @LastModifiedBy
     private String updatedBy;
 
 }
